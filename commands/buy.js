68ea2b3892e62feb3c980102c1fac41d39e10355
Glitch (shard-lizard-entree) <none>
2021-12-28 11:20:42 +0000

    🎏🌮 Checkpoint ./commands/sell.js:5606966/66 ./commands/buy.js:5606966/191

--- commands/buy.js
+++ commands/buy.js
@@ -5,11 +5,13 @@ exports.execute = async (client, message, args) => {
   if (userBalance.amount < 1)
     return message.channel.send("Looks like you are poor.");
   let item = args[0];
+  let count = args[1];
+  let count2=1;
   if (!item) return message.channel.send("What are you trying to buy?");
   let hasItem = client.shop[item];
   if (!hasItem || hasItem == undefined)
     return message.reply("That item doesnt exists lol");
-  let isBalanceEnough = userBalance.amount >= hasItem.cost;
+  let isBalanceEnough = userBalance.amount >= hasItem.cost*count2;
   if (!isBalanceEnough)
     return message.reply(
       "Your balance is insufficient. You need :dollar: " +
@@ -17,14 +19,16 @@ exports.execute = async (client, message, args) => {
         " to buy this item."
     );
   let buy = client.eco.removeMoney(message.author.id, hasItem.cost);
-
   let itemStruct = {
     name: item.toLowerCase(),
     prize: hasItem.cost
   };
+  while(count2<=count){
  client.db.push(`items_${message.author.id}`, itemStruct);
+    count2++;
+  }
   return message.channel.send(
-    `You purchased **${item}** for **:dollar: ${hasItem.cost}**.`
+    `**${count2}**tane **${item}** aldın **:dollar: ${hasItem.cost}**.`
   );
 };
 
