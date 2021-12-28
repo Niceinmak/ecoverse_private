1071a94454a235e4d53302ca6e407d66a9fc63e5
Glitch (shard-lizard-entree) <none>
2021-12-28 15:43:24 +0000

    🍬⛵️ Checkpoint ./commands/sell.js:5606966/1543

--- commands/sell.js
+++ commands/sell.js
@@ -4,7 +4,7 @@ exports.execute = async (client, message, args) => {
   let userBalance = client.eco.fetchMoney(message.author.id);
   let item = args[0];
   let quantity=0;
-  
+  let randomcash = Math.floor(Math.random() * 200);
   
   //------------------------------------------
     const x = client.db.get(`items_${message.author.id}`);
@@ -13,13 +13,14 @@ exports.execute = async (client, message, args) => {
     return itemStruct;
   }, {});
    client.db.delete(`items_${message.author.id}`)
+  //if(k=="yaygın.kasa") randomcash = Math.floor(Math.random() * 200);
   const result = Object.keys(arrayToObject).map(k =>
-  
       message.channel.send(`**${k} Kasasını Sattın** ve **${arrayToObject[k]*100}**💶 kazandın.`)
+                                                
   );
     const result2 = Object.keys(arrayToObject).map(k =>
   
-     quantity=arrayToObject[k]*100
+     
   );
   let sell = client.eco.addMoney(message.author.id, quantity);
    
