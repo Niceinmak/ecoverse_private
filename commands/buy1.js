2ca72dd37e2a79be678cbe70b6558950ef69653f
Glitch (shard-lizard-entree) <none>
2021-12-28 07:54:56 +0000

    💐⛱ Checkpoint ./commands/buy1.js:5606966/1843

--- commands/buy1.js
+++ commands/buy1.js
@@ -2,43 +2,31 @@ const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
   let userBalance = client.eco.fetchMoney(message.author.id);
-  if (userBalance.amount < 1)
-    return message.channel.send("Looks like you are poor.");
   let item = args[0];
-  if (!item) return message.channel.send("What are you trying to buy?");
-  let hasItem = client.shop[item];
-  if (!hasItem || hasItem == undefined)
-    return message.reply("That item doesnt exists lol");
-  let isBalanceEnough = userBalance.amount >= hasItem.cost;
-  if (!isBalanceEnough)
-    return message.reply(
-      "Your balance is insufficient. You need :dollar: " +
-        hasItem.cost +
-        " to buy this item."
-    );
-  let sell = client.eco.addMoney(message.author.id, hasItem.cost);
-  let itemStruct = {
-    name: item.toLowerCase(),
-    prize: hasItem.cost
-  };
+  let quantity=0;
+  
+  
   //------------------------------------------
     const x = client.db.get(`items_${message.author.id}`);
   const arrayToObject = x.reduce((itemStruct, x) => {
     itemStruct[x.name] = (itemStruct[x.name] || 0) + 1;
     return itemStruct;
   }, {});
+   client.db.delete(`items_${message.author.id}`)
   const result = Object.keys(arrayToObject).map(k =>
-                                                
-      message.channel.send(
-    `You sell **${item}** for **:dollar: ${arrayToObject[k]}**.`
-  )
+  
+      message.channel.send(`**$Kasalarını Sattın** ve **${arrayToObject[k]*100}**💶 kazandın.`)
   );
-  //--------------------------------------------------
+    const result2 = Object.keys(arrayToObject).map(k =>
+  
+     quantity=arrayToObject[k]*100
+  );
+  let sell = client.eco.addMoney(message.author.id, quantity);
    
 };
 
 exports.help = {
   name: "sell",
   aliases: [],
-  usage: `sell <item>`
+  usage: `sell`
 };
