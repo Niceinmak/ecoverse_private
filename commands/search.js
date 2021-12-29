6112f75d3f2ce8d2c640c70d4c9693e370e91086
Glitch (shard-lizard-entree) <none>
2021-12-29 08:22:23 +0000

    🐄📷 Checkpoint ./commands/search.js:5606966/1

--- commands/search.js
+++ commands/search.js
@@ -8,7 +8,7 @@ exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 200) + 50;
     let beg = await client.eco.beg(message.author.id, amount, { canLose: true, cooldown: 300000, customName: "search" });
     if (beg.onCooldown) return message.reply(`${beg.time.minutes} dakika & ${beg.time.seconds} saniye sonra tekrar gel.`);
-    if (beg.lost) return message.channel.send(`**${users[Math.floor(Math.random() * users.length)]}:**Yakalandın! Parayı alamadın ufaklık.`);
+    if (beg.lost) return message.channel.send(`**${users[Math.floor(Math.random() * users.length)]}:** Yakalandın! Parayı alamadın ufaklık.`);
     else return message.reply(`**${users[Math.floor(Math.random() * users.length)]}** ile karlı çıktın,**${beg.amount}**💶 para buldun. Şuan ki paran:**${beg.after}** 💶.`);
 };
 
