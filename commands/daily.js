c191d4490ea3dcdc19fdc8f19b5ede87736c8b7c
Glitch (shard-lizard-entree) <none>
2021-12-30 08:44:58 +0000

    ⚡️⛑ Checkpoint ./commands/daily.js:5606966/1 ./commands/work.js:5606966/26

--- commands/daily.js
+++ commands/daily.js
@@ -2,7 +2,7 @@ module.exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 500) + 100;
     let addMoney = client.eco.daily(message.author.id, amount);
     if (addMoney.onCooldown) return message.reply(`Bugünkü paranı topladın.Lütfen ${addMoney.time.hours} saat, ${addMoney.time.minutes} dakika & ${addMoney.time.seconds} saniye sonra tekrar gel.`);
-    else return message.reply(`Bu gün**${addMoney.amount}** 💸topladın.Toplam paran **${addMoney.after}** 💸oldu!`);
+    else return message.reply(`Bu gün **${addMoney.amount}** 💸topladın.Toplam paran **${addMoney.after}** 💸oldu!`);
 };
 
 module.exports.help = {
--- commands/work.js
+++ commands/work.js
@@ -13,7 +13,7 @@ module.exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 1500) + 1000;
     let work = client.eco.work(message.author.id, amount);
     if (work.onCooldown) return message.reply(`Çok yoruldum dostum.${work.time.minutes} dakika & ${work.time.seconds} sonra tekrar gel.`);
-    else return message.reply(`**${jobs[Math.floor(Math.random() * jobs.length)]}** olarak çalıştın ve **${work.amount}** 💶 . Now you have **${work.after}** 💶 .`);
+    else return message.reply(`**${jobs[Math.floor(Math.random() * jobs.length)]}** olarak çalıştın ve **${work.amount}** 💶 .**${work.after}** 💶 kazandın.`);
 };
 
 module.exports.help = {
