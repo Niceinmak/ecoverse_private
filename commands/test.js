044617962e33f9a934f28904d144dd3b6f6b51fc
Glitch (shard-lizard-entree) <none>
2021-12-30 06:27:55 +0000

    🎌🍶 Checkpoint ./events/ready.js:5606966/344 ./commands/test2.js:5606966/1140 ./commands/test.js:5606966/60

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
     const embed = new MessageEmbed()
-        .setTitle("Test Başarılı!")
+        .setTitle(`${message.guild.memberCount}`)
     return message.channel.send(embed);
 }
 
