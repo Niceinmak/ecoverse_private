2ab83926d8cb8a6527d38602820fa9320125872b
Glitch (shard-lizard-entree) <none>
2021-12-30 06:39:29 +0000

    💳🙇 Checkpoint ./events/ready.js:5606966/393 ./commands/test2.js:5606966/160

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
-  client.user.setActivity(`[𝙦 𝙝𝙚𝙡𝙥 ] ,${client.members}, , ${client.guilds.cache.size} sunucuya hizmet veriliyor. Niceinmak tarafından hazırlandı`);
+  client.user.setActivity(`[𝙦 𝙝𝙚𝙡𝙥 ] ,${guild.members.filter(member => !member.user.bot).size}, , ${client.guilds.cache.size} sunucuya hizmet veriliyor. Niceinmak tarafından hazırlandı`);
 };
