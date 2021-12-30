4fc096796b12a4d660b2c7d9c4c4b94a91514393
Glitch (shard-lizard-entree) <none>
2021-12-30 06:16:36 +0000

    🎣💂 Checkpoint ./events/ready.js:5606966/421

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
-  client.user.setActivity(`[BETA] cna ama fosworlu tarafından tasarlandı.𝙆𝙤𝙢𝙪𝙩𝙡𝙖𝙧 𝙞𝙘̧𝙞𝙣: 𝙦 𝙝𝙚𝙡𝙥 ${client.guilds.cache.size} sunucuya hizmet veriliyor.`);
+  client.user.setActivity(`[𝙦 𝙝𝙚𝙡𝙥 ] , ${client.guilds.cache.size} sunucuya hizmet veriliyor. Niceinmak tarafından hazırlandı`);
 };
