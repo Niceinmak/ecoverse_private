98f7554c1c40c8980eeb7aaa2c384e927db7b3ba
Glitch (shard-lizard-entree) <none>
2022-01-06 17:25:36 +0000

    🎐😅 Checkpoint ./events/ready.js:5606966/2 ./commands/leaderboard.js:5606966/359

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -6,7 +6,7 @@ exports.execute = async (client, message, args) => {
     if (!leaderboard || leaderboard.length < 1) return message.channel.send("❌ | Empty Leaderboard!");
     const embed = new MessageEmbed()
       //  .setAuthor(`${message.guild.name} ! Sunucusunun Liderler Sıralaması`, message.guild.iconURL)
-    .setAuthor(`Sofiath Top 15 Leaderboard`, message.guild.iconURL)
+    .setAuthor(`EcoVerse Top 15 Leaderboard`, message.guild.iconURL)
         .setColor("RANDOM")
         .setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.cache.get(leaderboard[0].id).displayAvatarURL : "https://cdn.discordapp.com/avatars/603948445362946084/a_f61398e073d78ae104e32b0517c891c3.gif")
         .setTimestamp();
