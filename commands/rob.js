311fa403f3f506543600e08b8c28ba8b77fe402c
Glitch (shard-lizard-entree) <none>
2022-01-06 20:22:51 +0000

    🌖🛤 Checkpoint ./commands/rob.js:5606966/198

--- commands/rob.js
+++ commands/rob.js
@@ -8,16 +8,16 @@ exports.execute = async (client, message, args) => {
     `You failed robbing ${target} becase the avengers got you`
   ]
    let amount = Math.floor(Math.random() * 50) + 10;
+  if(amount>(target.id).amount) amount=((target.id).amount)
     let rob = client.eco.beg(client.ecoAddUser, amount, { canLose: true });
-    if (rob.onCooldown) return message.reply(`**You have recently attempted to rob someone try again after ${rob.time.seconds} seconds.**`);
+    if (rob.onCooldown) return message.reply(`**${target.amount}You have recently attempted to rob someone try again after ${rob.time.seconds} seconds.**`);
     if (rob.lost) return message.channel.send(messages[Math.floor(Math.random() * messages.length)]);
     else { 
-      if(amount>(target.id).amount) amount=(target.id).amount
       client.eco.removeMoney(target.id, parseInt(amount));
-    //  let x = client.eco.fetchMoney(target.id).amount - amount 
-      client.eco.addMoney(message.author.id, parseInt(amount));
-     // client.eco.setMoney(target.id,parseInt(x))
-      message.reply(`**You robbed ${target}** for **${rob.amount}** 💶. Now you have **${rob.after}** 💶.`);
+     // let x = client.eco.fetchMoney(target.id).amount - amount 
+     client.eco.addMoney(message.author.id, parseInt(amount));
+     //client.eco.setMoney(target.id,parseInt(x))
+      message.reply(`**${target.amount}You robbed ${target}** for **${rob.amount}** 💶. Now you have **${rob.after}** 💶.`);
     }
 };
 
