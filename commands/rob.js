5fcbcec64cba22b3efc3493dfac0036265726e14
Glitch (shard-lizard-entree) <none>
2022-01-06 20:34:29 +0000

    👝🎺 Checkpoint ./commands/rob.js:5606966/719

--- commands/rob.js
+++ commands/rob.js
@@ -1,6 +1,7 @@
 exports.execute = async (client, message, args) => {
   let target = message.mentions.members.first() || message.guild.members.cache.get(args[0]) 
   let authordata = client.eco.fetchMoney(message.author.id) 
+  let targetBalance = client.eco.fetchMoney(target.id);
   if(!target) return message.reply("Who are you trying to rob?")
   let messages = [
     `You tripped while trying to rob ${target} and got caught!`,
@@ -8,16 +9,17 @@ exports.execute = async (client, message, args) => {
     `You failed robbing ${target} becase the avengers got you`
   ]
    let amount = Math.floor(Math.random() * 50) + 10;
-  if(amount>(target.id).amount) amount=((target.id).amount)
+  if(targetBalance.amount=="0") return message.reply(`**The person you're stealing has no money left.**`);
+  if(amount>targetBalance.amount) amount=(targetBalance.amount)-1
     let rob = client.eco.beg(client.ecoAddUser, amount, { canLose: true });
-    if (rob.onCooldown) return message.reply(`**${target.amount}You have recently attempted to rob someone try again after ${rob.time.seconds} seconds.**`);
+    if (rob.onCooldown) return message.reply(`**You have recently attempted to rob someone try again after ${rob.time.seconds} seconds.**`);
     if (rob.lost) return message.channel.send(messages[Math.floor(Math.random() * messages.length)]);
     else { 
-      client.eco.removeMoney(target.id, parseInt(amount));
-     // let x = client.eco.fetchMoney(target.id).amount - amount 
-     client.eco.addMoney(message.author.id, parseInt(amount));
-     //client.eco.setMoney(target.id,parseInt(x))
-      message.reply(`**${target.amount}You robbed ${target}** for **${rob.amount}** 💶. Now you have **${rob.after}** 💶.`);
+     // client.eco.removeMoney(target.id, parseInt(amount));
+      let x = client.eco.fetchMoney(target.id).amount - amount 
+     //client.eco.addMoney(message.author.id, parseInt(amount));
+     client.eco.setMoney(target.id,parseInt(x))
+      message.reply(`**You robbed ${target}** for **${rob.amount}** 💶. Now you have **${rob.after}** 💶.`);
     }
 };
 
