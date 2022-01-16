0b4e622ce15f1a9bce04d2ce4b050eaa730c9b2b
Glitch (sofiath1-12) <none>
2022-01-16 09:01:43 +0000

    👓⛽️ Checkpoint ./index.js:5606966/36 ./botConfig.js:5606966/83

--- botConfig.js
+++ botConfig.js
@@ -1,9 +1,8 @@
 module.exports = {
-    token: "OTI0MzExMDkyNDY4MDE1MTE2.YcctyQ.GQe_kpgDyocIOc-PYe5H0CrCAIU",
     prefix: "q ",
     admins: [
         "405247101442719764"
 ],
-    debug: true,
+    debug: false,
     countChannel: "countChannelID"
 };
--- index.js
+++ index.js
@@ -43,4 +43,4 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-client.login(client.config.token);
+client.login(process.env.TOKEN);
