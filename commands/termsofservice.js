5d18f5fb9648d510c9418bd6d454f794e1b8fe6f
Glitch (sofiath1-12) <none>
2022-01-29 13:47:37 +0000

    🏡🎏 Checkpoint ./commands/termsofservice.js:5606966/3571

--- commands/termsofservice.js
+++ commands/termsofservice.js
@@ -3,10 +3,11 @@ const { MessageButton } = require('discord-buttons')
 exports.execute = async (client, message, args) => {
   let userid=message.author.id
     const embed = new MessageEmbed()
-        .setAuthor("Privacy Policy")
-        .setTitle("EcoVerse Privacy Policy")
+        .setAuthor("Terms Of Use & Privacy Policy")
+        .setTitle("EcoVerse Terms Of Use & Privacy Policy")
      //   .setURL("https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ")
-        .setDescription(`**What information is stored?**
+        .setDescription(`**EcoVerse Privacy Policy**
+**What information is stored?**
 If you have any money, your user ID is stored. Your user ID can be stored multiple times for money..
 Money data, market data, user id and similar things are stored on our servers.
 
@@ -23,10 +24,41 @@ You can come to our discord server to learn more: https://discord.gg/2n9Zg9BGgY
 If you wish to delete your data, please contact us: https://discord.gg/2n9Zg9BGgY
 
 
-*Note: We reserve the right to change this without notifying our users.
-As soon as you add the bot to the server, you are deemed to have accepted the privacy policy*
+***Note: We reserve the right to change this without notifying our users.
+As soon as you add the bot to the server, you are deemed to have accepted the privacy policy***
 
-\`This policy was last updated January 6th, 2022.\``)
+\`This policy was last updated January 6th, 2022.
+Niceinmak#9634\`
+
+**EcoVerse Terms Of Service**
+These terms of service enable the use of the EcoVerse bot and access to its services. Please read these Terms carefully and contact us if you have any questions. By using our services, you agree to these terms and our privacy policy.
+
+
+**Your Content**
+The bot may receive this information, including your EcoVerse profile picture, username, your id and avatar. Everything you post or otherwise make available on our Services (commands) is referred to as "User Content".
+You reserve all rights to and are solely responsible for any user content you submit to EcoVerse.
+ 
+ 
+**How Long Do We Store Your Content?**
+We keep all data and information until you come to the help server and ask for your account to be closed. You consent to use your content as long as you use it under normal conditions and to change the amount of money you use in games within the bot and derivative works.
+
+​
+
+**Feedback You Provide**
+We value hearing from our users and are always trying to improve our EcoVerse bot. If you choose to post comments, ideas or feedback, you agree that we are free to use them without any restriction or compensation to you.
+
+​
+
+**Security**
+We care about the safety of our users. As we work to protect the security of your content and account, EcoVerse cannot guarantee that unauthorized third parties will not be able to defeat our security measures.
+
+​
+
+**Third-party links and services**
+EcoVerse does not contain any third party software. All data is stored on its own server.
+
+\`EcoVerse\`
+`)
         .setColor("BLURPLE")
         .setTimestamp()
         .setThumbnail(client.user.displayAvatarURL)
@@ -88,7 +120,7 @@ exports.execute = async (client, message, args) => {
 }
 
 exports.help = {
-    name: "privacy",
-    aliases: ["PRIVACY"],
-    usage: `privacy`
+    name: "termsofuse",
+    aliases: ["TERMSOFUSE"],
+    usage: `termsofuse`
 }
