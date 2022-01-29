f0c0e2bf5ef7848e121e0be3919b191c5863f980
Glitch (sofiath1-12) <none>
2022-01-29 13:23:38 +0000

    📼🚛 Checkpoint ./events/ready.js:5606966/282

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
   let serverformat=String(client.guilds.cache.size).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   let userformat=String(client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)).replace(/(.)(?=(\d{3})+$)/g,'$1,')
-  client.user.setActivity(`[𝙦 𝙝𝙚𝙡𝙥] , ${serverformat} servers and ${userformat} users are served.`);
+  client.user.setActivity(`EcoVerse | q help`);
 };
