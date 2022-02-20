06a2cf5cf1db1e968b31db7d9a1d8c9fe1f35722
Glitch (sofiath1-12) <none>
2022-02-20 15:43:58 +0000

    🍓🏐 Checkpoint ./commands/blackjack.js:5606966/22484

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -281,13 +281,58 @@ function stopbj(){
  // let argString = itemname.substr( itemname.indexOf(' ') + 1 );
   let agr1u=argString2[0]
   let agr2u=argString2[1]
-  if(userpoint<=21)
+  if(userpoint<=21 && userpoint<=16)
     {
-      embed.fields = [];
+      if(userpoint<=17){
+           embed.fields = [];
   embed.addFields(
     { name: `Dealer \`${agr1d}\``, value: agr2d, inline: true },
     { name: `${user1.username} \`${agr1u}\``, value: agr2u, inline: true },
 	)
+      }
+      else
+        {
+          let points=`${stopbj()}`
+      var argString = points.substring(1).split(' ');
+ // let argString = itemname.substr( itemname.indexOf(' ') + 1 );
+  let dpoint=argString[0]
+  let upoint=argString[1]
+  let dcard=argString[2]
+  let ucard=argString[3]
+  embed.fields = [];
+  embed.addFields(
+    { name: `Dealer \`${dpoint}\``, value: dcard, inline: true },
+    { name: `${user1.username} \`${upoint}\``, value: ucard, inline: true },
+	)
+   if(userpoint>dealerpoint)
+        {
+         embed.setAuthor(`You Win!`) 
+          embed.setFooter(`You win ${amount3}`)
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
+        }
+      else if(userpoint==dealerpoint)
+        {
+          embed.setFooter(`You both bust!`)
+          embed.setFooter(`You both bust!`)
+        }
+      else
+        {
+          if(dealerpoint>21)
+            {
+              embed.setAuthor(`You Win!`) 
+              embed.setFooter(`You win ${amount3}`)
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
+            }
+          else
+            {
+           embed.setAuthor(`You Lose`) 
+              embed.setFooter(`You lose ${amount3}`)
+          let data2= client.eco.removeMoney(messageid, parseInt(amount3));
+            }
+        }
+      return msg.edit(embed);
+        }
+   
     }
    else
       {
