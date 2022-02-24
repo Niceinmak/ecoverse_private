2b665c77874b622fa5987ffe156b6fe2b1974f0a
Glitch (sofiath1-12) <none>
2022-02-24 17:10:31 +0000

    🐢💕 Checkpoint ./botConfig.js:5606966/71 ./commands/addmoney.js:5606966/181 ./commands/setmoney.js:5606966/5101

--- botConfig.js
+++ botConfig.js
@@ -1,7 +1,8 @@
 module.exports = {
     prefix: "q ",
     admins: [
-        "405247101442719764"
+        "405247101442719764",
+        "766192311004037120",
 ],
     debug: false,
     countChannel: "countChannelID"
