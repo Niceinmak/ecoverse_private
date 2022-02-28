c8b64c931c5c25968068223b3e51750c1acad117
Glitch (sofiath1-12) <none>
2022-02-28 14:46:09 +0000

    🙈⏳ Checkpoint ./botConfig.js:5606966/154

--- botConfig.js
+++ botConfig.js
@@ -4,6 +4,7 @@ module.exports = {
         "405247101442719764",
         "766192311004037120",
         "601064482076360704",
+        "500355407730966540",
 ],
     debug: false,
     countChannel: "countChannelID"
