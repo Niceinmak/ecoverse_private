d241be901c1a7bcc0a516fa7779b11e41ee06a25
Glitch (sofiath1-12) <none>
2022-03-08 16:19:52 +0000

    🏵🌟 Checkpoint ./commands/botinfo.js:5606966/4350

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,5 +1,25 @@
 const { MessageEmbed } = require("discord.js");
 exports.execute = async (client, message, args) => {
+  let id=""
+  for(let i=0;i<30;i++)
+    {
+      let temp=`${args[i]}`
+      if(temp!=`undefined`)
+        {
+          let temp2=`${args[i+1]}`
+          if(temp2!=`undefined`)
+            {
+        id+=`${args[i]} `
+            }
+          else
+            {
+        id+=`${args[i]}`}
+        }
+      else
+        {
+          break;
+        }
+    }
     if (!client.config.admins.includes(message.author.id)) return message.reply("**Only bot administrators.**"); // return if author isn't bot owner
   let serverformat=String(client.guilds.cache.size).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   let userformat=String(client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)).replace(/(.)(?=(\d{3})+$)/g,'$1,')
@@ -9,9 +29,15 @@ const embed = new MessageEmbed()
        .addField(`Users:`,userformat)
 let servernames=""
 client.guilds.cache.forEach(guild => {
-  servernames+=`\`${guild.name} | ${guild.id}\`\n`
+  if(guild.id==id)
+    {
+   embed.addField(`Find Server:`,`\`${guild.name} | ${guild.id}\``)
+    }
+  if(guild.name==id)
+    {
+   embed.addField(`Find Server:`,`\`${guild.name} | ${guild.id}\``)
+    }
 })
-    embed.setDescription(`Server Names:\n${servernames}`)
   message.channel.send(embed);
   
 }
