b142ee299e5d879c1cdff022f99514097abf5aa1
Glitch (sofiath1-12) <none>
2022-03-15 16:23:14 +0000

    🐢💗 Checkpoint ./index.js:5606966/348

--- index.js
+++ index.js
@@ -147,6 +147,9 @@ client.on("ready", () => {
   let cmd = new DiscordSlash.CommandBuilder();
 cmd.setName("exCmdGuild");
 cmd.setDescription("exCmdGuild Desc");
-slash.create(cmd, "832661859xxxx" /* Guild ID */);
+slash.create(cmd, "925628280785231872" /* Guild ID */);
+  slash.get(null, "925628280785231872").then((res) => {
+        console.log(res);
+    })
 })
 client.login(process.env.TOKEN);
